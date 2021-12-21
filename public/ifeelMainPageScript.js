e3695b961d4316cec47c699e988e5b7d5a7178aa
Hibinger <82664046+Hibinger@users.noreply.github.com>
2021-12-21 16:55:45 +0000

    prompts loading on main page from db

--- public/ifeelMainPageScript.js
+++ public/ifeelMainPageScript.js
@@ -1,13 +1,16 @@
 
-/*/-------- | This is the ifeel Main Page JS File |---------/*/
+//CREATE THE PROMPTS TEXT
 
-/* When the user clicks on the button,
-toggles between hiding and showing the dropdown content */
+function createPromptHTML(prompt) {
+  return "<a onclick='changePrompt(); myFunction()'; id='prompt' tag='" + prompt.promptid + "'>" + prompt.prompt_txt + "</a>";
+}
+
+//SHOW THE PROMPTS 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// From W3Schools - Filtered Dropdown
+//FILTER THE PROMPTS
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
@@ -25,6 +28,7 @@ function filterFunction() {
   }
 }
 
+//CHANGE THE PROMPT NAME ON THE TOP
 function changePrompt(){
 
   var hasbeenclicked = true
@@ -51,10 +55,11 @@ const imageArray =["url('img/Image1.png')","url('img/Image2.jpeg')", "url('img/I
 "url('img/Image8.jpeg')","url('img/Image9.jpeg')","url('img/Image10.jpeg')"];
 
 
-window.onload = function() {
-const gridArray = [2,3,4,5,6,7,8,9,10,11,12]; //Array that contains the grid items. Eventually this would need to be updated automatically when users post.
-var originalcontainer = document.getElementById('container'); // The grid container.
-var originalgrid = document.getElementById('1'); // The original grid element that is duplicated every time. 
+window.onload = async function() {
+
+  const gridArray = [2,3,4,5,6,7,8,9,10,11,12]; //Array that contains the grid items. Eventually this would need to be updated automatically when users post.
+  var originalcontainer = document.getElementById('container'); // The grid container.
+  var originalgrid = document.getElementById('1'); // The original grid element that is duplicated every time. 
 
   for (var i = 0; i < gridArray.length; i++){ // The loop that clones the original grid item according to the Array. 
     var newPost = originalgrid.cloneNode(true);
@@ -66,6 +71,28 @@ var originalgrid = document.getElementById('1'); // The original grid element th
   for(var i = 0; i < imageArray.length; i++){
     document.getElementById(i+1).style.backgroundImage = imageArray[i]; // adds the image to the grid items. 
   }
+
+  //AJAX CODE FOR THE PROMPTS
+
+  let promptsDropdown = document.getElementById("myDropdown");
+    try {
+        let prompts = await $.ajax({
+            url: "/api/Prompt",
+            method: "get",
+            dataType: "json"
+        });
+        let html = "";
+        console.log("[prompts] prompts = " + JSON.stringify(prompts));
+        for (let prompt of prompts) {
+            console.log("[prompts] prompts = " + JSON.stringify(prompt));
+            html += createPromptHTML(prompt);
+        }
+        promptsDropdown.innerHTML += html;
+
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 // var modal = document.querySelector('.bg-modal');
