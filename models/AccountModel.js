88a982408a26aba3f430978c66b81aee1ce57df1
Hibinger <82664046+Hibinger@users.noreply.github.com>
2022-01-04 22:04:36 +0000

    drawing page changed

--- models/AccountModel.js
+++ models/AccountModel.js
@@ -3,7 +3,7 @@
 var pool = require("./connection");
 module.exports.getAccounts = async function() {
     try {
-        let sql = "select * from Account"
+        let sql = "select * from Account";
         let result = await pool.query(sql);
         let account = result.rows;
         console.log("[AccountModel] products = " + JSON.stringify(account));
