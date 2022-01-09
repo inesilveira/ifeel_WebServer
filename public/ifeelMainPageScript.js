713a91724fefd6670cd1cd751f6445958ac8f3dc
inesilveira <inesfos@live.com.pt>
2022-01-09 17:09:29 -0100

    Revert "Merge branch 'Mariya' into Inês"
    This reverts commit c344363a1d046760d49664856540737e89bb4021, reversing
    changes made to 89dee0abdd8d66b8cf812efb0fdb38a6e343774a.

--- public/ifeelMainPageScript.js
+++ public/ifeelMainPageScript.js
@@ -1,24 +1,26 @@
+/*  STORE THE USER ID WHEN LOGGING IN ---- MOVE THIS TO THE LOGIN PAGE JS ----- */
+  function login(){
+     sessionStorage.setItem('userid', 1);
+   } 
 
-//-------- | This is the ifeel Main Page JS File |---------//
+ //  this should be inside the onload ---- MOVE THIS TO THE LOGIN PAGE JS -----
+// login();
 
-//--Side bar
-/* This opens the sidebar. Sets the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "180px";
+
+
+//CREATE THE PROMPTS TEXT
+
+
+function createPromptHTML(prompt) {
+  return "<a onclick='changePrompt(); myFunction()'; id='prompt' tag='" + prompt.promptid + "'>" + prompt.prompt_txt + "</a>";
 }
 
-/* This closes the sidebar. Sets the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-} 
-
-/* When the user clicks on the button, it
-toggles between hiding and showing the dropdown content (prompt menu) */
+//SHOW THE PROMPTS 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// From W3Schools - Filtered Dropdown
+//FILTER THE PROMPTS
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
@@ -36,6 +38,7 @@ function filterFunction() {
   }
 }
 
+//CHANGE THE PROMPT NAME ON THE TOP
 function changePrompt(){
 
   var hasbeenclicked = true
@@ -57,16 +60,16 @@ function changePrompt(){
 }
 
 //Image Array to store images that are displayed on the main page.
-const imageArray =["url('img/Image1.png')","url('img/Image2.png')", "url('img/Image3.png')",
-"url('img/Image4.png')","url('img/Image5.png')","url('img/Image6.png')","url('img/Image7.png')",
-"url('img/Image8.png')","url('img/Image9.png')","url('img/Image10.png')","url('img/Image11.png')"
-,"url('img/Image12.png')","url('img/Image13.png')"];
+const imageArray =["url('img/Image1.png')","url('img/Image2.jpeg')", "url('img/Image3.jpeg')",
+"url('img/Image4.jpeg')","url('img/Image5.jpeg')","url('img/Image6.jpeg')","url('img/Image7.jpeg')",
+"url('img/Image8.jpeg')","url('img/Image9.jpeg')","url('img/Image10.jpeg')"];
 
 
-window.onload = function() {
-const gridArray = [2,3,4,5,6,7,8,9,10,11,12,13]; //Array that contains the grid items. Eventually this would need to be updated automatically when users post.
-var originalcontainer = document.getElementById('container'); // The grid container.
-var originalgrid = document.getElementById('1'); // The original grid element that is duplicated every time. 
+window.onload = async function() {
+
+  const gridArray = [2,3,4,5,6,7,8,9,10,11,12]; //Array that contains the grid items. Eventually this would need to be updated automatically when users post.
+  var originalcontainer = document.getElementById('container'); // The grid container.
+  var originalgrid = document.getElementById('1'); // The original grid element that is duplicated every time. 
 
   for (var i = 0; i < gridArray.length; i++){ // The loop that clones the original grid item according to the Array. 
     var newPost = originalgrid.cloneNode(true);
@@ -78,7 +81,35 @@ var originalgrid = document.getElementById('1'); // The original grid element th
   for(var i = 0; i < imageArray.length; i++){
     document.getElementById(i+1).style.backgroundImage = imageArray[i]; // adds the image to the grid items. 
   }
+
+  //AJAX CODE FOR THE PROMPTS
+
+  let promptsDropdown = document.getElementById("myDropdown");
+
+  login();
+  try {
+      let prompts = await $.ajax({
+          url: "/api/Prompt",
+          method: "get",
+          dataType: "json"
+      });
+      let html = "";
+      console.log("[prompts] prompts = " + JSON.stringify(prompts));
+      for (let prompt of prompts) {
+          console.log("[prompts] prompts = " + JSON.stringify(prompt));
+          html += createPromptHTML(prompt);
+      }
+      promptsDropdown.innerHTML += html;
+
+  } catch (err) {
+      console.log(err);
+  }
+
 }
+
+// var modal = document.querySelector('.bg-modal');
+// var btn = document.getElementById('1');
+
 
 var currentId; // Stores the id that is clicked on. 
 
