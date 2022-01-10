129b71007d205c38842538d4b6f3b8901b15758c
Hibinger <82664046+Hibinger@users.noreply.github.com>
2022-01-10 16:24:11 +0000

    idk what this is

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,17 @@
 var pg = require('pg');
 
+<<<<<<< HEAD
 const connectionString = "postgres://postgres:admin@localhost:5432/ifeel_database"
 console.log("connectionString = " + connectionString);
+=======
+const connectionString = "postgres://postgres:BeautifulLife24@localhost:5432/ifeel_database"
+console.log("connectionsString = " + connectionString);
+>>>>>>> parent of ec83d76 (Merge branch 'main' into Mariya)
 
 const Pool = pg.Pool
 const pool = new Pool({
-    connectionString,
-    max: 10
+        connectionString,
+        max: 10
 })
 
 module.exports = pool;
